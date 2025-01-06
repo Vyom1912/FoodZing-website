8c7031f29e6f25e46952874498aead5e007367a5
VYOM PATEL <vyom1912@gmail.com>
2025-01-06 09:44:03 +0530

    Initial commit

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,7 +1,7 @@
-import React, { useContext } from "react";
+import { StoreContext } from "../../context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
 import "./FoodDisplay.css";
-import { StoreContext } from "../../context/StoreContext";
+import { useContext } from "react";
 
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
@@ -10,16 +10,20 @@ const FoodDisplay = ({ category }) => {
     <div className='food-display' id='food-display'>
       <h2>Our Signature dishes</h2>
       <div className='food-display-list'>
-        {food_list.map((item, index) => (
-          <FoodItem
-            key={index}
-            id={item._id}
-            name={item.name}
-            price={item.price}
-            description={item.description}
-            image={item.image}
-          />
-        ))}
+        {food_list.map((item, index) => {
+          if (category === "All" || category === item.category) {
+            return (
+              <FoodItem
+                key={index}
+                id={item._id}
+                name={item.name}
+                price={item.price}
+                description={item.description}
+                image={item.image}
+              />
+            );
+          }
+        })}
       </div>
     </div>
   );
